1258fbc4bb3e1ab6889560022c9dedb0e767cfa7
David Becker <beckerd8@mail.uc.edu>
2025-01-21 13:39:26 -0500

    Added an A before class name.

--- InClass20250121-4010-001/mainPackage/a026.py
+++ InClass20250121-4010-001/mainPackage/a026.py
@@ -3,5 +3,5 @@
 import random
 random_number = random.randint(1, 100)
 print(random_number, end=" ")
-print("| David Becker - Does the loss agaisnt Michigan make them the best team in the league?")
+print("| David Becker ")
 
