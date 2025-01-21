48ad518b535fbbabba3e7391015ac317ce626a00
JPowell2003 <75809385+JPowell2003@users.noreply.github.com>
2025-01-21 13:11:05 -0500

    Added a random number generator to module 013

--- InClass20250121-4010-001/mainPackage/013.py
+++ InClass20250121-4010-001/mainPackage/013.py
@@ -1,1 +1,19 @@
+# File Name : InClass20250121-4010-001
+# Student Name: Jay Powell
+# email: powela9@mail.uc.edu
+# Assignment Number: In Class Assignment 1/21/2025
+# Due Date: N/A
+# Course #/Section: IS4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Practicing GitHub Pulls, Forks, Clones, and Pushes in class
+# Brief Description of module: Generates a random number between 1 and 100 and prints my name on the same line
+# Citations: W3Schools
+# Anything else that's relevant:
+
+import random
+
+random_int = random.randint(1, 100)
+print(random_int, end=" ")
+print("Jay")
+
 
