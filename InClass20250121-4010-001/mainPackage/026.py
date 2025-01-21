896f310a149920b6a08d1b0f3cc4b001347ec848
David Becker <beckerd8@mail.uc.edu>
2025-01-21 13:10:56 -0500

    Added random number and name.

--- InClass20250121-4010-001/mainPackage/026.py
+++ InClass20250121-4010-001/mainPackage/026.py
@@ -1,1 +1,7 @@
+# Correct documentation
+
+import random
+random_number = random.randint(1, 100)
+print(random_number, end=" ")
+print("| David Becker - Does the loss agaisnt Michigan make them the best team in the league?")
 
