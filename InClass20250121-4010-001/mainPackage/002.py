d831263e0bd6bba73866c30f949024db0a57f78b
Jacob Brumfield <brumfijb@mail.uc.edu>
2025-01-21 13:10:15 -0500

    Added a print statement that prints a random number as well as my name

--- InClass20250121-4010-001/mainPackage/002.py
+++ InClass20250121-4010-001/mainPackage/002.py
@@ -1,1 +1,16 @@
+#002.py
+#Name: jacob brumfield
+#Email: brumfijb@mail.uc.edu
+#Assignment Number: In class 20250121
+#Due Date: 1/21/2025
+#Course #/Section: IS 4010 001
+#Semester/Year: Spring 2025
+#Brief Description: Prints random integer as well as your name
+#What this mod does: Prints an integer and my name
+
+
+import random
+
+random_number = random.randint (1,10000)
+print (random_number, "Jacob Brumfield") 
 
